b3d22476eebeca45821e5f84abc69116ab8282f3
nagatomo <nagatomo@aaaa.com>
2022-04-08 13:29:00 +0900

    コンポーネントに分けた

---
+++ src/components/incompleted_area.jsx
@@ -0,0 +1,21 @@
+export const IncompletedArea = (props) => {
+    const { incompTodos, toggle, remove } = props;
+  
+    return (
+      <div className="incomplete_area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompTodos.map((todo, index) => {
+            return (
+              <li key={index} className="list_row">
+                <div>{todo}</div>
+                <button onClick={() => toggle(index)}>完了</button>
+                <button onClick={() => remove(index)}>削除</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+  
---
+++ src/components/input_area.jsx
@@ -0,0 +1,11 @@
+export const InputArea = (props) => {
+    const { inputText, onChange, onClick } = props;
+  
+    return (
+      <div className="input_area">
+        <input placeholder="TODOを入力" value={inputText} onChange={onChange} />
+        <button onClick={onClick}>追加</button>
+      </div>
+    );
+  };
+  
